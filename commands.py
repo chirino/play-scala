ca52215a10e234667b6c09fc2468bc9f7cafab48
Guillaume Bort <guillaume.bort@gmail.com>
2010-01-31 13:21:28 +0100

    Eclipsify with scala

--- commands.py
+++ commands.py
@@ -13,7 +13,14 @@ if play_command == 'scala:console':
   	subprocess.call(java_cmd, env=os.environ)
      	print
 	sys.exit(0)
+	
 # ~~~~~~~~~~~~~~~~~~~~~~ New
 if play_command == 'new':
 	os.remove(os.path.join(application_path, 'app/controllers/Application.java'))
 	shutil.copyfile(os.path.join(play_base, 'modules/scala/resources/Application.scala'), os.path.join(application_path, 'app/controllers/Application.scala'))
+
+# ~~~~~~~~~~~~~~~~~~~~~~ Eclipsify
+if play_command == 'ec' or play_command == 'eclipsify':
+	dotProject = os.path.join(application_path, '.project')
+	replaceAll(dotProject, r'org\.eclipse\.jdt\.core\.javabuilder', "ch.epfl.lamp.sdt.core.scalabuilder")
+	replaceAll(dotProject, r'<natures>', "<natures>\n\t\t<nature>ch.epfl.lamp.sdt.core.scalanature</nature>")
