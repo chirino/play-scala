ea845bad23004e47b6f7f8cd0c295e1f71c30f67
peter hausel <peter_kovac11@hotmail.com>
2010-02-07 10:33:07 -0500

    fixing scala console

--- src/play/console/Console.scala
+++ src/play/console/Console.scala
@@ -1,8 +1,10 @@
 package play.console
-import scala.tools.nsc.MainGenericRunner
+import scala.tools.nsc._
 import java.io.File
 import play.Play
 import play.db.jpa.JPAPlugin
+import jline.ConsoleReader
+
 object Console {
    def main(args : Array[String]) {
      val root = new File(System.getProperty("application.path"));
@@ -13,10 +15,17 @@ object Console {
      println("~")
      Play.start()
      JPAPlugin.startTx(false)  
-     //TODO:fix readline support
-     //now that the app is ready, launch scala REPL  
-     try {	
-       MainGenericRunner.main(args)
+     try {
+       //launch readline loop using play's classloader
+       val command = new GenericRunnerCommand(Nil, (error:String) => println(error))
+       JLine.withJLine {
+            val loop = new InterpreterLoop {
+              override def createInterpreter() = {
+                super.createInterpreter()
+              }
+            }
+            loop.main(command.settings)
+        }     
      } catch {
 	case e:Exception=>
      }
@@ -24,4 +33,31 @@ object Console {
      JPAPlugin.closeTx(false)
      exit(0)
    }
+}
+
+/**
+* lifted from <a href="http://github.com/harrah/sbt/blob/master/src/main/scala/sbt/LineReader.scala">sbt</a>
+* credit goes to Mark Harrah 
+**/
+private object JLine
+{
+  def terminal = jline.Terminal.getTerminal
+  def createReader() =
+    terminal.synchronized
+    {
+      val cr = new ConsoleReader
+      terminal.enableEcho()
+      cr.setBellEnabled(false)
+      cr
+    }
+  def withJLine[T](action: => T): T =
+  {
+    val t = terminal
+    t.synchronized
+    {
+      t.disableEcho()
+      try { action }
+      finally { t.enableEcho() }
+    }
+  }
 }
